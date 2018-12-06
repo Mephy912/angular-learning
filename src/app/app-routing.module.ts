d754213acfc7fb9be72ff9f0f400155e03789f1c
Mephy912 <qwaszx8411@gmail.com>
2018-12-06 14:08:22 +0800

    add navigation component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,14 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
+import { NavigationComponent } from './navigation/navigation.component';
+
 const routes: Routes = [
-    { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
-    { path: 'heroes', component: HeroesComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: 'detail/:id', component: HeroDetailComponent }
+    { path: '', component: NavigationComponent }
+    // { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+    // { path: 'heroes', component: HeroesComponent },
+    // { path: 'dashboard', component: DashboardComponent },
+    // { path: 'detail/:id', component: HeroDetailComponent }
 ];
 
 @NgModule({
